aa51a15e088c0bd27df867729a194daa40c4f498
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-12-23 06:38:26 +0000

    add code if

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-alert('Hello World');
+let hello="Hello World";
+alert(hello);
+
+// let int1= 1;
+// alert(int1);
+
+// let int2= -10;
+// alert(int2);
+
+// let float1= 3.14;
+// alert(float1);
+
+// let str1= "JavaScriptを覚えよう";
+// alert(str1);
+
+// alert(4 + 3);
+
+// alert(8 - 5);
+
+// alert(2 * 6);
+
+// alert(10 / 2);
+
+// let str1="Hello";
+// let str2="World!!";
+// alert(str1 + str2);
+
+let orange = 100;
+let apple = 120;
+
+if(orange<apple){
+  alert("みかんの値段がりんごより安い");
+} else if(orange==apple){
+  alert("みかんとりんごが同じ値段");
+}else{
+  alert("みかんの値段がりんごより高い");
+}
