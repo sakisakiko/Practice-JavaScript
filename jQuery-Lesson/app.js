f33f0ed3b982d66f21bb9b706a77826aeb7e96a4
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-12-28 11:24:55 +0000

    add mouseover and mouseout

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -19,12 +19,25 @@
 //   });
 // });
 
+// [5章-確認問題]jQueryでHTMLのCSS要素を操作しよう
+// $(function(){
+//   $('.box1').slideDown(function (){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp();
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
-  });
-});
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000FF'})
+  })
+})
+
+$(function(){
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#FF0000'})
+  })
+})
